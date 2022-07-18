d110824b3ba0a59a3b0e51e457c026a9db28fb43
Leo Dupire <leo.dupire@brocc.se>
2022-07-18 01:18:05 -0400

    Added Resume and cleaned up Home page a little

--- src/Home.js
+++ src/Home.js
@@ -6,11 +6,14 @@ import LEO from "./images/LEO.jpg";
 
 const Home = () => {
 
+    const whoAmI = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    const whatIDo = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+
     const Tab = (props) => {
         return (
             <div className="Tab">
                 <Link to={props.link}>
-                    <p id="Home" data-hover={props.title}>{props.title}</p>
+                    <p id="HomeTab" data-hover={props.title}>{props.title}</p>
                 </Link>
             </div>
         );
@@ -29,10 +32,10 @@ const Home = () => {
                             />
                         </div>
                         <div className="Name" id="First">
-                            <h2 id="Home">Léo</h2>
+                            <h2 id="HomeName">Léo</h2>
                         </div>
                         <div className="Name" id="Last">
-                            <h2 id="Home">Dupire</h2>
+                            <h2 id="HomeName">Dupire</h2>
                         </div>
                         <div className="FirstNameBack"></div>
                         <div className="LastNameBack"></div>
@@ -61,11 +64,16 @@ const Home = () => {
                     </div>
                 </div>
                 <div className="body" id="Home">
-                    <h2>About me</h2>
-                    <h3>Who I am</h3>
-                    <h3>What I do</h3>
-                    <h2>Documents!</h2>
-                    <h3>My résumé</h3>
+                    <h2 id="Home">About me</h2>
+                    <h3 id="Home">Who I am</h3>
+                    <p id="Home">{whoAmI}</p>
+                    <h3 id="Home">What I do</h3>
+                    <p id="Home">{whatIDo}</p>
+                    <h2 id="Home">Documents!</h2>
+                    <h3 id="Home">My résumé</h3>
+                    <div className="ResumePosition">
+                        <iframe src="https://drive.google.com/file/d/12vix--qu-DkUbIdiGD_drLmiRTw9i-BB/preview" allow="autoplay" className="ResumePDF"></iframe>
+                    </div>
                 </div>
             </div>
         </>
