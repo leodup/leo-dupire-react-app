4e70fb28437086b2b1a9c088fe73b19ced4848bc
Leo Dupire <leo.dupire@brocc.se>
2022-07-19 00:52:15 -0400

    Added gallery images Home

--- src/Home.js
+++ src/Home.js
@@ -3,8 +3,29 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import LEO from "./images/LEO.jpg";
+import Hyperloop from "./images/Hyperloop.jpg";
+import DAC from "./images/DAC.jpg";
+import Alphabite from "./images/Alphabite.jpg";
+import LawnCycle from "./images/LawnCycle.jpg";
+import Monta from "./images/Monta.jpg";
 
 const Home = () => {
+
+    const [count, setCount] = useState(0);
+    const gallery = [Hyperloop, DAC, Alphabite, LawnCycle, Monta];
+    const galleryCurrent = gallery[count];
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCount(prevCount => prevCount + 1) // new
+            if(count > gallery.length - 2) // I'n not entirely sure why -2
+            {
+                setCount(prevCount => prevCount - prevCount)
+            }
+        }, 10000);
+
+        return () => clearInterval(interval);
+    })
 
     const whoAmI = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     const whatIDo = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
@@ -39,7 +60,13 @@ const Home = () => {
                         </div>
                         <div className="FirstNameBack"></div>
                         <div className="LastNameBack"></div>
-                        <div className="LargeExpand"></div>
+                        <div className="LargeExpand">
+                            <img
+                                className="imageHyperloop"
+                                src={galleryCurrent}
+                                alt="Hyperloop Team"
+                            />
+                        </div>
                     </div>
                     <div className="Tabs">
                         <Tab
