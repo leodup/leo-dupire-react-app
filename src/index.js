e9874bf7b3e5dad02d6c2475760e55b17f9261ee
Léo Dupire <leodupire@10-17-225-33.dynapool.wireless.nyu.edu>
2023-02-21 16:15:44 -0500

    Trying to fix reload page bug

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App+/App';
@@ -7,7 +7,7 @@ import App from './App+/App';
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
